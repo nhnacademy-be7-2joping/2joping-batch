10b2a3442f2a9ad5556051f5bd92cc4b42fab254
choeunlee <choeun1500@gmail.com>
2024-10-24 17:06:52 +0900

    feat: setup project

---
+++ src/main/java/com/nhnacademy/twojopingbatch/TwojopingBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.twojopingbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojopingBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwojopingBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/twojopingbatch/TwojopingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.twojopingbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwojopingBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
